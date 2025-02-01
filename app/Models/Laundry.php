7771d61b62f7997dd7be0b23934a12a1b6201797
faturh <fikrihabib25@gmail.com>
2025-02-01 18:29:35 +0700

    update model + migration

---
+++ app/Models/Laundry.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class laundry extends Model
+{
+    //
+    use HasFactory;
+    protected $fillable=[
+
+    'claim_code',
+    'user_id',
+    'shop_id',
+    'weight',
+    'with_pickup',
+    'with_delivery',
+    'pickup_address',
+    'delivery_address',
+    'total',
+    'description',
+    'status'
+
+    ];
+
+}
---
+++ app/Models/Shop.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class shop extends Model
+{
+    //
+    use HasFactory;
+
+    protected $fillable = [
+        'image',
+        'name',
+        'location',
+        'city',
+        'delivery',
+        'pickup',
+        'whatsapp',
+        'description',
+        'price',
+        'rate',
+    ];
+}
